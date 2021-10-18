996fac276b1099bbbe5d0ed8d887cbe38a3f5a3c
Nick Rupert <nick@tyrannosaurustech.com>
2021-10-18 16:39:06 -0400

    Register ModelflakeMapCommand

--- src/Providers/SnowflakeProvider.php
+++ src/Providers/SnowflakeProvider.php
@@ -3,11 +3,16 @@
 namespace Moves\Snowflake\Providers;
 
 use Carbon\Laravel\ServiceProvider;
+use Moves\Snowflake\Console\Commands\ModelflakeMapCommand;
 
 class SnowflakeProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->commands([
+            ModelflakeMapCommand::class
+        ]);
+
         $this->publishes([
             __DIR__ . '/../config/snowflake.php' => config_path('snowflake.php'),
         ]);
